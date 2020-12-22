a46ef4cd8494c3e932cc998f4f9d9e6ff71bf216
justus.klameth <justus.klameth@outlook.de>
2020-12-22 13:52:45 +0100

    Add url shortener

--- routes/filedrop-router.js
+++ routes/filedrop-router.js
@@ -15,8 +15,13 @@ var router = express.Router()
 router.get('/:filedrop', (req, res) => {
     for (filedrop of filedrops) {
         if (filedrop.name === req.params.filedrop) {
-            res.render('collection', {...filedrop, pageBranding: options.branding, version: version})
-            return  
+            if (filedrop.type === 'file') {
+                res.render('collection', {...filedrop, pageBranding: options.branding, version: version})
+                return
+            } else if (filedrop.type === 'url') {
+                res.redirect(filedrop.url);
+                return
+            }
         }
     }
     res.status(404).send(`Die angeforderte Ressource "${req.params.filedrop}" konnte nicht gefunden werden.`)
